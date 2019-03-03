979d9164d0f154624d1af28e61214ddfc252c230
GRHP92 <polychronakis.h@gmail.com>
2019-03-03 20:25:47 +0100

    Adding the start file

---
+++ start.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def main():
+    # Generate the window
+    win = tk.Tk()
+    # Change attributes of the window
+    win.title("Calculator")
+    # Run the window
+    win.mainloop()
+
+if __name__ == "__main__":
+    main()
